a42d814ba7abd88dd964448fbd008f6b30356d35
jiavu <customerservice@CSGermanyiMac3.fritz.box>
2019-01-04 17:23:43 +0100

    almost finished.
    client.invoke() bug

--- build/assets/main2.js
+++ build/assets/main2.js
@@ -2,13 +2,37 @@
 // https://developer.zendesk.com/apps/docs/apps-v2
 
 
-const url = "https://help.magine.com/hc/de/";
-let endpoint = "search?utf8=✓&query=";      // plus Suchbegriff.
-
+const startpoint = "https://help.magine.com/hc/de/";
+const endpointPrefix = "search?utf8=✓&query=";      // plus Suchbegriff.
+//search?utf8=%E2%9C%93&query= // after encodeURI
 
 let client = ZAFClient.init();
-client.invoke('resize', { width: '100%', height: '115px' });
+client.invoke('resize', { width: '100%', height: '115px' });  // causes an error if submitting form 2nd time.
 
 
+jQuery(function($) {
+
+    $("#form").submit(function() {
+    //$("#button").click(function() {
+
+        let url = startpoint;
+        let endpoint;
+
+        if ( $("#search-input").val() ) {
+            endpoint = endpointPrefix + $("#search-input").val();
+            url = startpoint + endpoint;
+        }
+
+        window.open( encodeURI(url), "_blank" );
+
+        $("#search-input").val("");
+
+    });
+
+});
+
+
+/* ////////////////////////////// */
 
 //encodeURIComponent(eingabe);
+//encodeURI(eingabe);
