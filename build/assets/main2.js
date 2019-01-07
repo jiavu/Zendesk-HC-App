ba18546a725b39ae05b98457d0c0f21c4b8c4a05
jiavu <cs@CS-Germany-iMac.fritz.box>
2019-01-07 18:10:12 +0100

    Finished
    <form> causes a bug of client.invoke('resize', {}) if submitting 2nd time.
    
    "client.invoke is not a function."
    
    <form> isn't used anymore now.

--- build/assets/main2.js
+++ build/assets/main2.js
@@ -7,26 +7,36 @@ const endpointPrefix = "search?utf8=✓&query=";      // plus Suchbegriff.
 //search?utf8=%E2%9C%93&query= // after encodeURI
 
 let client = ZAFClient.init();
-client.invoke('resize', { width: '100%', height: '115px' });  // causes an error if submitting form 2nd time.
+client.invoke('resize', { width: '100%', height: '121px' });
+/* ^causes an error if submitting form 2nd time. client.invoke is not a function.
+    I think it has something to do with my using of <form> in html.
+    Working without it now. Can't use .onsubmit() so I have to use a key event listener for Enter/Return key.
+*/
 
 
+function openHC() {
+    let url = startpoint;
+    let endpoint;
+
+    if ( $("#search-input").val() ) {
+        endpoint = endpointPrefix + $("#search-input").val();
+        url = startpoint + endpoint;
+    }
+
+    window.open( encodeURI(url), "_blank" );
+
+    $("#search-input").val("");
+}
+
+// event listening:
 jQuery(function($) {
 
-    $("#form").submit(function() {
-    //$("#button").click(function() {
-
-        let url = startpoint;
-        let endpoint;
-
-        if ( $("#search-input").val() ) {
-            endpoint = endpointPrefix + $("#search-input").val();
-            url = startpoint + endpoint;
-        }
-
-        window.open( encodeURI(url), "_blank" );
-
-        $("#search-input").val("");
-
+    $("#button").click(openHC);
+    //$("#form").submit(openHC);
+    //$("#search-input").submit(openHC);
+        
+    $("#search-input").on('keyup', function (e) {
+        if (e.keyCode == 13) { openHC() }
     });
 
 });
